fd0246d117d9ca3237f5fed9fad70603ea7244ed
A-DDONG <ssc5924@gmail.com>
2023-08-04 09:21:02 +0900

    todo-03

---
+++ react-001-todolist-03/src/data/SampleTodo.js
@@ -0,0 +1,38 @@
+const SampleData = [
+  {
+    id: 0,
+    sdate: "2023-08-03",
+    stime: "10:00:00",
+    content: "React 학습하기",
+    complete: true,
+  },
+  {
+    id: 1,
+    sdate: "2023-08-03",
+    stime: "10:23:46",
+    content: "TodoList",
+    complete: true,
+  },
+  {
+    id: 2,
+    sdate: "2023-08-03",
+    stime: "11:00:22",
+    content: "계란먹는 날",
+    complete: false,
+  },
+  {
+    id: 3,
+    sdate: "2023-08-03",
+    stime: "17:30:20",
+    content: "집에 가고싶은 날",
+    complete: false,
+  },
+  {
+    id: 4,
+    sdate: "2023-08-03",
+    stime: "22:13:27",
+    content: "놀고 싶은 날",
+    complete: true,
+  },
+];
+export { SampleData };
