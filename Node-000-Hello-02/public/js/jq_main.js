ccce65ea7ac7825b36f6392223e20d48cb3e6630
A-DDONG <ssc5924@gmail.com>
2023-08-07 15:47:32 +0900

    20230807

---
+++ Node-000-Hello-02/public/js/jq_main.js
@@ -0,0 +1,23 @@
+// 지금부터 여기는 DOM HTML JS 영역이다 영역이다
+// document.addEventListener("DOMContentLoaded", () => {});
+
+// 지금부터 여기는 JQuery Script 영역이다
+$(document).ready(function () {});
+$(function () {});
+$(() => {
+  $("div.home").html("반갑습니다");
+  $("div.bbs").css("color", "blue");
+  //   const divs1 = document.querySelectorAll("div.main");
+  //   const onClickHandler = (e) => {
+  //     const text = e.currentTarget.innerText;
+  //     alert(text);
+  //   };
+  //   for (let i = 0; i < divs1.length; i++) {
+  //     divs1[i].addEventListener(onClickHandler);
+  //   }
+  const divs = $("div.main");
+  divs.on("click", function (e) {
+    const text = this.innerText;
+    alert(text);
+  });
+});
--- Node-001-bbs-01/README.md
+++ Node-001-bbs-01/README.md
@@ -12,3 +12,20 @@
 npm install -g sequelize-auto
 npm install -g mysql2
 ```
+
+## 기존에 생성된 DB Scheme 로 부터 DB 정보 가져오기
+
+`sequelize-auto -d imageDB -h localhost -u root -x '!Biz8080'`
+
+# models 폴더의 model 파일들 수정
+
+- tbl\*.js파일들 수정
+- init-models.js 파일 수정
+
+## 서버 폴더에서 node_modules 다시 생성(다운로드 실행)
+
+`npm install`
+
+## 서버 start
+
+`nodemon`
